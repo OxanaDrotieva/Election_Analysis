568672c7553453aaf75011a842b45bcef3683762
Oxana <oxanadrotieva@gmail.com>
2021-11-16 23:23:12 -0600

    Adding a PyPoll_Challenge.py

--- resources/PyPoll.py
+++ resources/PyPoll.py
@@ -24,7 +24,7 @@
 import csv
 import os
 # Assign a variable for the file to load and the path.
-file_to_load = os.path.join("resourses", "election_results.csv")
+file_to_load = os.path.join("resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
